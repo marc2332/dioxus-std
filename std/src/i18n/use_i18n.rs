b9026cf478e7816a7d39f23b6a214cb32030ed49
Evan Almloff <evanalmloff@gmail.com>
2024-02-08 11:30:36 -0600

    fix mutability for i18n

--- std/src/i18n/use_i18n.rs
+++ std/src/i18n/use_i18n.rs
@@ -106,7 +106,7 @@ impl UseI18 {
         self.translate_with_params(id, HashMap::default())
     }
 
-    pub fn set_language(&self, id: LanguageIdentifier) {
+    pub fn set_language(&mut self, id: LanguageIdentifier) {
         *self.selected_language.write() = id;
     }
 }
